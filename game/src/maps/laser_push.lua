4e28d057af0bf3ad6c9aaed64a19b5596651b589
Luke <lukethefroh@gmail.com>
2021-09-05 03:25:39 +0800

    laser maze

--- game/src/maps/laser_push.lua
+++ game/src/maps/laser_push.lua
@@ -5,11 +5,11 @@ return {
   orientation = "orthogonal",
   renderorder = "right-down",
   width = 16,
-  height = 16,
+  height = 17,
   tilewidth = 16,
   tileheight = 16,
   nextlayerid = 4,
-  nextobjectid = 13,
+  nextobjectid = 31,
   properties = {},
   tilesets = {
     {
@@ -286,7 +286,7 @@ return {
       x = 0,
       y = 0,
       width = 16,
-      height = 16,
+      height = 17,
       id = 1,
       name = "map",
       visible = true,
@@ -298,22 +298,23 @@ return {
       properties = {},
       encoding = "lua",
       data = {
-        1, 2, 3, 4, 5, 2, 3, 4, 5, 2, 3, 4, 5, 2, 3, 6,
-        11, 12, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 13, 14, 15, 6,
-        21, 22, 9, 18, 8, 17, 29, 29, 10, 18, 9, 28, 20, 10, 20, 16,
-        31, 22, 19, 19, 17, 27, 19, 17, 20, 10, 29, 30, 10, 7, 9, 26,
-        11, 22, 20, 20, 7, 30, 17, 28, 9, 18, 30, 8, 8, 10, 30, 36,
-        21, 22, 10, 20, 18, 27, 19, 29, 28, 30, 20, 8, 27, 7, 8, 16,
-        31, 22, 20, 18, 17, 28, 17, 28, 29, 28, 30, 7, 10, 10, 8, 26,
-        11, 22, 9, 20, 20, 19, 8, 7, 9, 8, 19, 28, 27, 17, 19, 36,
-        21, 22, 19, 10, 7, 18, 19, 27, 18, 27, 10, 28, 3, 28, 3, 16,
-        31, 22, 8, 8, 20, 10, 18, 30, 18, 27, 19, 17, 3, 7, 3, 26,
-        11, 22, 20, 29, 27, 17, 19, 18, 28, 23, 27, 8, 3, 7, 3, 36,
-        21, 22, 19, 27, 18, 7, 18, 9, 30, 23, 19, 18, 3, 9, 3, 16,
-        31, 22, 18, 29, 29, 30, 27, 8, 27, 23, 24, 24, 3, 23, 24, 26,
-        11, 22, 27, 20, 18, 20, 10, 18, 28, 20, 28, 19, 23, 23, 20, 36,
-        1, 32, 33, 34, 33, 34, 33, 34, 33, 34, 3, 3, 3, 18, 7, 16,
-        41, 42, 43, 44, 45, 42, 43, 44, 45, 42, 43, 44, 45, 42, 43, 46
+        79, 79, 79, 11, 5, 2, 3, 4, 5, 2, 3, 4, 5, 24, 3, 6,
+        79, 79, 79, 1, 13, 14, 13, 14, 13, 14, 13, 14, 13, 25, 101, 6,
+        79, 79, 79, 31, 8, 17, 29, 23, 10, 18, 9, 28, 20, 51, 79, 16,
+        79, 79, 79, 11, 17, 27, 19, 55, 54, 2, 2, 2, 2, 16, 79, 26,
+        79, 79, 79, 31, 7, 30, 17, 6, 1, 18, 30, 8, 8, 6, 79, 36,
+        79, 79, 79, 31, 18, 27, 19, 36, 31, 30, 20, 8, 27, 16, 79, 16,
+        79, 79, 79, 11, 17, 28, 17, 3, 2, 24, 24, 24, 24, 3, 79, 26,
+        79, 79, 79, 11, 20, 19, 8, 7, 9, 8, 19, 28, 24, 15, 79, 36,
+        79, 79, 79, 41, 45, 43, 42, 45, 45, 54, 10, 55, 56, 28, 51, 46,
+        79, 79, 79, 79, 79, 79, 79, 79, 79, 11, 19, 36, 31, 7, 16, 79,
+        79, 79, 79, 79, 79, 79, 79, 79, 79, 11, 27, 16, 11, 7, 16, 79,
+        79, 79, 79, 79, 79, 79, 11, 70, 16, 11, 19, 16, 11, 9, 2, 26,
+        79, 79, 79, 79, 79, 1, 2, 37, 2, 2, 24, 3, 4, 23, 14, 26,
+        79, 79, 79, 79, 79, 1, 10, 18, 28, 20, 28, 19, 23, 23, 20, 36,
+        79, 79, 79, 79, 79, 1, 33, 34, 33, 34, 24, 51, 54, 18, 7, 16,
+        79, 79, 79, 79, 79, 41, 43, 44, 45, 42, 43, 46, 21, 18, 19, 26,
+        79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 41, 45, 42, 46
       }
     },
     {
@@ -321,7 +322,7 @@ return {
       x = 0,
       y = 0,
       width = 16,
-      height = 16,
+      height = 17,
       id = 3,
       name = "scatter",
       visible = true,
@@ -336,8 +337,9 @@ return {
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 78, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 69, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -364,22 +366,6 @@ return {
       parallaxy = 1,
       properties = {},
       objects = {
-        {
-          id = 7,
-          name = "Mirror",
-          type = "",
-          shape = "rectangle",
-          x = 192,
-          y = 208,
-          width = 16,
-          height = 16,
-          rotation = 0,
-          gid = 2147483750,
-          visible = true,
-          properties = {
-            ["initialFacingDir"] = "UpLeft"
-          }
-        },
         {
           id = 8,
           name = "ExitDoor",
@@ -409,8 +395,22 @@ return {
           properties = {}
         },
         {
-          id = 12,
-          name = "Crate",
+          id = 13,
+          name = "Light",
+          type = "",
+          shape = "rectangle",
+          x = 208,
+          y = 192,
+          width = 16,
+          height = 16,
+          rotation = 0,
+          gid = 87,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 15,
+          name = "Mirror",
           type = "",
           shape = "rectangle",
           x = 208,
@@ -418,7 +418,117 @@ return {
           width = 16,
           height = 16,
           rotation = 0,
-          gid = 84,
+          gid = 3221225574,
+          visible = true,
+          properties = {
+            ["initialFacingDir"] = "DownLeft"
+          }
+        },
+        {
+          id = 22,
+          name = "Mirror",
+          type = "",
+          shape = "rectangle",
+          x = 224,
+          y = 240,
+          width = 16,
+          height = 16,
+          rotation = 0,
+          gid = 2147483750,
+          visible = true,
+          properties = {
+            ["initialFacingDir"] = "UpLeft"
+          }
+        },
+        {
+          id = 23,
+          name = "Mirror",
+          type = "",
+          shape = "rectangle",
+          x = 208,
+          y = 240,
+          width = 16,
+          height = 16,
+          rotation = 0,
+          gid = 102,
+          visible = true,
+          properties = {
+            ["initialFacingDir"] = "UpRight"
+          }
+        },
+        {
+          id = 26,
+          name = "Mirror",
+          type = "",
+          shape = "rectangle",
+          x = 224,
+          y = 112,
+          width = 16,
+          height = 16,
+          rotation = 0,
+          gid = 2147483750,
+          visible = true,
+          properties = {
+            ["initialFacingDir"] = "UpLeft"
+          }
+        },
+        {
+          id = 27,
+          name = "Mirror",
+          type = "",
+          shape = "rectangle",
+          x = 192,
+          y = 208,
+          width = 16,
+          height = 16,
+          rotation = 0,
+          gid = 1073741926,
+          visible = true,
+          properties = {
+            ["initialFacingDir"] = "DownRight"
+          }
+        },
+        {
+          id = 28,
+          name = "Mirror",
+          type = "",
+          shape = "rectangle",
+          x = 224,
+          y = 16,
+          width = 16,
+          height = 16,
+          rotation = 0,
+          gid = 3221225574,
+          visible = true,
+          properties = {
+            ["initialFacingDir"] = "DownLeft"
+          }
+        },
+        {
+          id = 29,
+          name = "Tomb-GoldenKey",
+          type = "",
+          shape = "rectangle",
+          x = 80,
+          y = 32,
+          width = 16,
+          height = 16,
+          rotation = 0,
+          gid = 85,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 30,
+          name = "Coffee",
+          type = "",
+          shape = "rectangle",
+          x = 224,
+          y = 208,
+          width = 16,
+          height = 16,
+          rotation = 0,
+          gid = 90,
           visible = true,
           properties = {}
         }
