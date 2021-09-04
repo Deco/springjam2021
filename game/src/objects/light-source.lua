dd4f6f594b6c95f2f6c27ff8d38d0607c343290d
Declan White <declan@respawn.com>
2021-09-05 03:30:30 +0800

    more mirror types, light propagation

--- game/src/objects/light-source.lua
+++ game/src/objects/light-source.lua
@@ -2,11 +2,9 @@ LightSource = Engine:EntityClass('LightSource')
 
 function LightSource:setup(data)
     self.image = Engine:getAsset('art/flashlight.png')
-    self.dir = self.dir or Cardinal.Right
-    self.initialDir = self.initialDir or Cardinal.Right
     self.illuminatedCellsList = self.illuminatedCellsList or { }
     self.firstRun = false--util.default(self.firstRun, false)
-    self.illuminatedMirrors = {}
+    self.illuminatedMirrors = self.illuminatedMirrors or {}
 
     BasicEntSetup(self, data)
 end
@@ -22,14 +20,15 @@ end
 
 function LightSource:update()
     if not self.firstRun then
-        self.initialDir = self.initialDir or self.dir
         self.firstRun = true
         self:updateLight()
     end
 end
 
 function LightSource:updateLight()
+    local lastLitTimeMap = {}
     for _, illuminatedCell in ipairs(self.illuminatedCellsList) do
+        lastLitTimeMap[illuminatedCell] = illuminatedCell.litBySet[self].time
         illuminatedCell.litBySet[self] = nil
         for mirror, source in pairs(illuminatedCell.directlyLitBySet) do
             if source == self then
@@ -43,35 +42,41 @@ function LightSource:updateLight()
 
     self.illuminatedCellsList = {}
     self.illuminatedMirrors = {}
-    self.dir = self.initialDir
 
-    local currPos = self:getPos() + math.cardinalToOffset(self.dir)
+    local currDir = 'from above!!'
+    local currPos = self:getPos()
     local currDirectLighter = self
-    for loopIdx = 1, 600 do
+    local animStepCounter = 0
+    for stepIdx = 1, 600 do
         local currCell = WORLD:getCell(currPos)
         table.insert(self.illuminatedCellsList, currCell)
-        currCell.litBySet[self] = true
+        currCell.litBySet[self] = { idx = animStepCounter, time = lastLitTimeMap[currCell] or GAMETIME }
+        if not lastLitTimeMap[currCell] then animStepCounter = animStepCounter + 1 end
         currCell.directlyLitBySet[currDirectLighter] = self
         local mirrors = currCell:findEntsOfClass(Mirror)
         if #mirrors > 0 then
-            local lightFromDir = math.indexWrap(self.dir + 2, 4)
+            local lightFromDir = type(currDir) == number and math.indexWrap(currDir + 2, 4) or currDir
             local newDir = mirrors[1]:redirectLight(lightFromDir)
             if newDir == nil then
                 break
             end
-            print("Im in a mirror" .. mirrors[1].facingDiagDir)
+            --print("Im in a mirror" .. mirrors[1].facingDiagDir)
 
-            print("Light From: " .. self.dir .. " -> " .. newDir)
-            self.dir = newDir
+            --print("Light From: " .. currDir .. " -> " .. newDir)
+            currDir = newDir
             mirrors[1].isReflecting = true
             table.insert(self.illuminatedMirrors, mirrors[1])
             currDirectLighter = mirrors[1]
         end
-        print("adding" .. currPos.x .. ", " .. currPos.y)
+        --print("adding" .. currPos.x .. ", " .. currPos.y)
         if not currCell:lightPassTest(self) and #currCell:findEntsOfClass(Mirror) < 1 then
             break
         end
-        currPos = currPos + math.cardinalToOffset(self.dir)
+        if type(currDir) == "number" then
+            currPos = currPos + math.cardinalToOffset(currDir)
+        else
+            break
+        end
     end
 end
 
