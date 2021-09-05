aaf37a77b68c20e20a992b0533ed3504b084b944
Keegan Ott <keegan.ott@dreeko.me>
2021-09-05 11:03:30 +0800

    empty room

---
+++ game/src/maps/empty_room.lua
@@ -0,0 +1,369 @@
+return {
+  version = "1.5",
+  luaversion = "5.1",
+  tiledversion = "1.7.2",
+  orientation = "orthogonal",
+  renderorder = "right-down",
+  width = 17,
+  height = 16,
+  tilewidth = 16,
+  tileheight = 16,
+  nextlayerid = 4,
+  nextobjectid = 37,
+  properties = {},
+  tilesets = {
+    {
+      name = "dungeon-tileset",
+      firstgid = 1,
+      tilewidth = 16,
+      tileheight = 16,
+      spacing = 0,
+      margin = 0,
+      columns = 10,
+      image = "../../art/tilesets/dungeon.png",
+      imagewidth = 160,
+      imageheight = 176,
+      objectalignment = "topleft",
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      grid = {
+        orientation = "orthogonal",
+        width = 16,
+        height = 16
+      },
+      properties = {},
+      wangsets = {},
+      tilecount = 110,
+      tiles = {
+        {
+          id = 0,
+          type = "wall"
+        },
+        {
+          id = 1,
+          type = "wall"
+        },
+        {
+          id = 2,
+          type = "wall"
+        },
+        {
+          id = 3,
+          type = "wall"
+        },
+        {
+          id = 4,
+          type = "wall"
+        },
+        {
+          id = 5,
+          type = "wall"
+        },
+        {
+          id = 6,
+          type = "floor"
+        },
+        {
+          id = 7,
+          type = "floor"
+        },
+        {
+          id = 8,
+          type = "floor"
+        },
+        {
+          id = 9,
+          type = "floor"
+        },
+        {
+          id = 10,
+          type = "wall"
+        },
+        {
+          id = 11,
+          type = "floor"
+        },
+        {
+          id = 12,
+          type = "floor"
+        },
+        {
+          id = 13,
+          type = "floor"
+        },
+        {
+          id = 14,
+          type = "floor"
+        },
+        {
+          id = 15,
+          type = "wall"
+        },
+        {
+          id = 16,
+          type = "floor"
+        },
+        {
+          id = 17,
+          type = "floor"
+        },
+        {
+          id = 18,
+          type = "floor"
+        },
+        {
+          id = 19,
+          type = "floor"
+        },
+        {
+          id = 20,
+          type = "wall"
+        },
+        {
+          id = 21,
+          type = "floor"
+        },
+        {
+          id = 22,
+          type = "floor"
+        },
+        {
+          id = 23,
+          type = "floor"
+        },
+        {
+          id = 24,
+          type = "floor"
+        },
+        {
+          id = 25,
+          type = "wall"
+        },
+        {
+          id = 26,
+          type = "floor"
+        },
+        {
+          id = 27,
+          type = "floor"
+        },
+        {
+          id = 28,
+          type = "floor"
+        },
+        {
+          id = 29,
+          type = "floor"
+        },
+        {
+          id = 30,
+          type = "wall"
+        },
+        {
+          id = 31,
+          type = "floor"
+        },
+        {
+          id = 32,
+          type = "floor"
+        },
+        {
+          id = 33,
+          type = "floor"
+        },
+        {
+          id = 34,
+          type = "floor"
+        },
+        {
+          id = 35,
+          type = "wall"
+        },
+        {
+          id = 36,
+          type = "wall"
+        },
+        {
+          id = 37,
+          type = "wall"
+        },
+        {
+          id = 40,
+          type = "wall"
+        },
+        {
+          id = 41,
+          type = "wall"
+        },
+        {
+          id = 42,
+          type = "wall"
+        },
+        {
+          id = 43,
+          type = "wall"
+        },
+        {
+          id = 44,
+          type = "wall"
+        },
+        {
+          id = 45,
+          type = "wall"
+        },
+        {
+          id = 50,
+          type = "wall"
+        },
+        {
+          id = 51,
+          type = "wall"
+        },
+        {
+          id = 52,
+          type = "wall"
+        },
+        {
+          id = 53,
+          type = "wall"
+        },
+        {
+          id = 54,
+          type = "wall"
+        },
+        {
+          id = 55,
+          type = "wall"
+        },
+        {
+          id = 60,
+          type = "floor"
+        },
+        {
+          id = 61,
+          type = "floor"
+        },
+        {
+          id = 62,
+          type = "floor"
+        },
+        {
+          id = 63,
+          type = "floor"
+        },
+        {
+          id = 70,
+          type = "floor"
+        },
+        {
+          id = 71,
+          type = "floor"
+        },
+        {
+          id = 72,
+          type = "floor"
+        },
+        {
+          id = 73,
+          type = "floor"
+        },
+        {
+          id = 78,
+          type = "pit"
+        },
+        {
+          id = 100,
+          type = "pit"
+        }
+      }
+    }
+  },
+  layers = {
+    {
+      type = "tilelayer",
+      x = 0,
+      y = 0,
+      width = 17,
+      height = 16,
+      id = 1,
+      name = "map",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      encoding = "lua",
+      data = {
+        1, 2, 3, 4, 5, 2, 3, 4, 5, 2, 3, 4, 5, 2, 4, 3, 6,
+        11, 28, 27, 29, 8, 19, 8, 28, 18, 9, 27, 19, 9, 28, 28, 7, 6,
+        21, 18, 7, 7, 8, 8, 19, 18, 8, 7, 29, 19, 17, 9, 19, 9, 16,
+        31, 8, 18, 27, 7, 19, 27, 7, 18, 20, 30, 8, 7, 18, 8, 9, 26,
+        11, 19, 27, 7, 17, 7, 30, 9, 19, 29, 8, 8, 28, 29, 9, 8, 36,
+        21, 18, 19, 18, 7, 8, 19, 29, 18, 17, 29, 29, 18, 29, 29, 9, 16,
+        31, 29, 9, 20, 27, 28, 8, 17, 17, 30, 8, 19, 19, 27, 27, 27, 26,
+        11, 28, 19, 28, 18, 7, 18, 29, 7, 10, 27, 20, 19, 18, 17, 27, 36,
+        21, 18, 18, 9, 9, 9, 27, 8, 8, 19, 28, 10, 17, 19, 19, 27, 16,
+        31, 27, 29, 8, 27, 8, 17, 9, 18, 8, 19, 8, 29, 19, 28, 7, 26,
+        11, 17, 9, 7, 7, 29, 29, 29, 18, 10, 28, 7, 18, 19, 9, 7, 36,
+        21, 18, 29, 28, 28, 8, 27, 28, 29, 19, 19, 28, 27, 29, 27, 9, 16,
+        31, 8, 27, 27, 18, 27, 19, 28, 27, 17, 9, 9, 29, 19, 27, 19, 26,
+        11, 27, 19, 17, 27, 17, 28, 18, 18, 17, 29, 9, 7, 17, 17, 9, 36,
+        1, 19, 9, 8, 29, 28, 7, 18, 7, 17, 17, 19, 19, 27, 29, 29, 16,
+        41, 42, 43, 44, 45, 42, 42, 44, 45, 42, 43, 44, 45, 42, 45, 43, 46
+      }
+    },
+    {
+      type = "tilelayer",
+      x = 0,
+      y = 0,
+      width = 17,
+      height = 16,
+      id = 3,
+      name = "scatter",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      encoding = "lua",
+      data = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+      }
+    },
+    {
+      type = "objectgroup",
+      draworder = "topdown",
+      id = 2,
+      name = "entities",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      objects = {}
+    }
+  }
+}
